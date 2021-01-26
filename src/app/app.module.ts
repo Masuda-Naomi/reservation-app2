2588d914ca9b14ab05e3ba29c999295359628a14
Masuda <n.masuda@fujinet.co.jp>
2021-01-26 14:41:24 +0900

    7-89

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+//import=輸入する（定義）
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
+//@NgModule=構成要素を登録
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +22,9 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//declarations=宣言
+//imports=その他(declarationsに入れなかったもの)
+//bootstrap=最初に起動するコンポーネントの指定
+
+//export class AppModeule{}=NgModuleの設定情報をAppModuleとして公開
